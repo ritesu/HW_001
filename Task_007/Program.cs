652409535a082e0961335bf368973c6033391bd6
ritesu <ritesu@mail.ru>
2023-01-16 15:56:49 +0900

    Task_007

---
+++ Task_007/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numbers = Enumerable.Range(10, 99).ToArray();
+int max = 0;
+for (int a = 10; a < numbers.Length; a++)
+{
+    if (a > max)
+    {
+        max = a;
+    }
+    numbers[a] = numbers[a] + 1;
+    
+}
+
+Console.WriteLine("Максимальное число");
+Console.Write(max);
