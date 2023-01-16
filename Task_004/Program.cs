9050c73840b50c9ee90ed3ecb63d101ca480ec93
ritesu <ritesu@mail.ru>
2023-01-16 14:53:01 +0900

    Task_004

---
+++ Task_004/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+
+for (int a = 1; a < 21; a++)
+{
+    int b = a % 2;
+    if (b == 0)
+    {
+        Console.WriteLine(a);
+    }
+    numbers[a] = numbers[a] + 1;
+    
+}
