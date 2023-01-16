0f43e819642b10aa720bcf23f692e15efc3f6a3c
ritesu <ritesu@mail.ru>
2023-01-16 16:00:44 +0900

    Task_008

---
+++ Task_008/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int a = int.Parse(Console.ReadLine()?? "0");
+while(a < 100 || a > 999)
+{
+    Console.WriteLine("Введено некорректное чило");
+    a = int.Parse(Console.ReadLine()?? "0");
+}
+int b = a % 10;
+int c = (a / 10) %10;
+int d = a / 100;
+int e = d * 10 + b;
+Console.WriteLine(e);
