76f0d3a04065e7f1c73fdcd356c2c72485a34c57
ritesu <ritesu@mail.ru>
2023-01-16 16:12:18 +0900

    Task_009

---
+++ Task_009/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите делимое");
+double a = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите делитель");
+double b = double.Parse(Console.ReadLine() ?? "0");
+double c = a % b;
+if (c == 0)
+{
+    Console.WriteLine("Делится нацело");
+}
+else
+{
+    Console.WriteLine("Нацело не делится");
+    Console.WriteLine("Остаток равен");
+    Console.WriteLine(c);
+}
