81a23526bef2490e345e1b206aeb56de518f4ee3
ritesu <ritesu@mail.ru>
2023-01-16 15:14:04 +0900

    Task_004

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -1,6 +1,6 @@
-﻿int[] numbers = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+﻿int[] numbers = Enumerable.Range(0, 100).ToArray();
 
-for (int a = 1; a < 21; a++)
+for (int a = 1; a < 101; a++)
 {
     int b = a % 2;
     if (b == 0)
