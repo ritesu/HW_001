9619e1cfb864f29b4719fd1b1cba18fd5999e81c
ritesu <ritesu@mail.ru>
2023-01-16 14:17:20 +0900

    Task_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Проверка числа на четность");
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()?? "0");
+int b = a % 2;
+if (b == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является не четным");
+}
