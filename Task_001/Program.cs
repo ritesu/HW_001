255b2dc634c5b624e45b6ccb82fd95db90612808
ritesu <ritesu@mail.ru>
2023-01-16 13:47:45 +0900

    Task_001

---
+++ Task_001/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Является ли первое число квадратом второго?");
+Console.WriteLine("Введите первое число");
+double a = double.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите второе число");
+double b = double.Parse(Console.ReadLine() ?? "0");
+double c = b / a;
+if (c == a)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
