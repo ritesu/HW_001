3335de5b0ca569e7a0a9ae422afb7091e001a5c7
ritesu <ritesu@mail.ru>
2023-01-28 18:46:14 +1100

    Correct task

--- Task_007/Program.cs
+++ Task_007/Program.cs
@@ -1,13 +1,16 @@
-﻿int[] numbers = Enumerable.Range(10, 99).ToArray();
+﻿int[] numbers = Enumerable.Range(10, 100).ToArray();
 int max = 0;
 for (int a = 10; a < numbers.Length; a++)
 {
-    if (a > max)
+    int b = numbers[a] / 10;
+    if (b > 0)
     {
-        max = a;
+        if (a > max)
+        {
+            max = a;
+        }
+        numbers[a] = numbers[a] + 1;
     }
-    numbers[a] = numbers[a] + 1;
-    
 }
 
 Console.WriteLine("Максимальное число");
